76102a926fb891ce0ef7416c203e88137761de78
Gustavo <s3898300@student.rmit.edu.au>
2024-09-13 12:56:13 +1000

    Setup typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test");
+console.log("hello");
+const chicken = new test_1.Chicken();
+chicken.cluck();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Chicken } from "./test"
+
+console.log("hello");
+
+const chicken = new Chicken();
+chicken.cluck();
